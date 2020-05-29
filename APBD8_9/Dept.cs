5c3d94a82f91b9fe9629f3941a96b7d3192d2c12
Piotr Bliżewski <piotr.blizewski@gmail.com>
2020-05-29 20:42:55 +0200

    done APBD 8/9

---
+++ APBD8_9/Dept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APBD8_9
+{
+    class Dept
+    {
+        public int Deptno { get; set; }
+        public string Dname { get; set; }
+        public string Loc { get; set; }
+    }
+}
---
+++ APBD8_9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APBD8_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linq = new LinqSamples();
+        }
+    }
+}
